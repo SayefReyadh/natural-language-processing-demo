bc4f90adbd71fcde41f54fd9ce71096e881183e6
Sayef Reyadh <sayef.reyadh.aust@gmail.com>
2018-10-16 01:06:40 +0600

    running different classifier from sklearn on movie reviews dataset.
    Result below :::
    Orginal Naive Bayes Classifier Algorithm Accuracy Percent :  85.0
    MultinomialNB Classifier Algorithm Accuracy Percent :  80.0
    BernoulliNB Classifier Algorithm Accuracy Percent :  76.0
    LogisticRegression Classifier Algorithm Accuracy Percent :  78.0
    SGDClassifier Classifier Algorithm Accuracy Percent :  76.0
    SVC Classifier Algorithm Accuracy Percent :  68.0
    LinearSVC Classifier Algorithm Accuracy Percent :  79.0
    NuSVC Classifier Algorithm Accuracy Percent :  76.0

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 import nltk
 import random
-from nltk.corpus import  movie_reviews
+from nltk.corpus import movie_reviews
 import pickle
+from nltk.classify.scikitlearn import SklearnClassifier
+
+import numpy
+import scipy
+import matplotlib
+import sklearn
+
+from sklearn.naive_bayes import MultinomialNB , GaussianNB , BernoulliNB
+from sklearn.linear_model import LogisticRegression,SGDClassifier
+from sklearn.svm import  SVC , LinearSVC ,NuSVC
 
 #documents = [(list(movie_reviews.words(fileid)) , category)
 #             for category in movie_reviews.categories()
@@ -59,7 +69,47 @@ testing_set = featuresets[1900:]
 
 classifier = load_classifier_from_pickle("nb.pickle")
 
-print("Naive Bayes Algorithm Accuracy Percent : " , (nltk.classify.accuracy(classifier,testing_set))*100)
-classifier.show_most_informative_features(15)
+print("Orginal Naive Bayes Classifier Algorithm Accuracy Percent : ", (nltk.classify.accuracy(classifier,testing_set))*100)
+# classifier.show_most_informative_features(15)
 
 # save_classifier_as_pickle(classifier)
+
+# MultinomialNB
+MultinomialNB_classifier = SklearnClassifier(MultinomialNB())
+MultinomialNB_classifier.train(training_set)
+print("MultinomialNB Classifier Algorithm Accuracy Percent : ", (nltk.classify.accuracy(MultinomialNB_classifier,testing_set))*100)
+
+# GaussianNB
+# GaussianNB_classifier = SklearnClassifier(GaussianNB())
+# GaussianNB_classifier.train(training_set)
+# print("GaussianNB Algorithm Accuracy Percent : ", (nltk.classify.accuracy(GaussianNB_classifier,testing_set))*100)
+
+# BernoulliNB
+BernoulliNB_classifier = SklearnClassifier(BernoulliNB())
+BernoulliNB_classifier.train(training_set)
+print("BernoulliNB Classifier Algorithm Accuracy Percent : ", (nltk.classify.accuracy(BernoulliNB_classifier,testing_set))*100)
+
+# LogisticRegression
+LogisticRegression_classifier = SklearnClassifier(LogisticRegression())
+LogisticRegression_classifier.train(training_set)
+print("LogisticRegression Classifier Algorithm Accuracy Percent : ", (nltk.classify.accuracy(LogisticRegression_classifier,testing_set))*100)
+
+# SGDClassifier
+SGDClassifier_classifier = SklearnClassifier(BernoulliNB())
+SGDClassifier_classifier.train(training_set)
+print("SGDClassifier Classifier Algorithm Accuracy Percent : ", (nltk.classify.accuracy(SGDClassifier_classifier,testing_set))*100)
+
+# SVC
+SVC_classifier = SklearnClassifier(SVC())
+SVC_classifier.train(training_set)
+print("SVC Classifier Algorithm Accuracy Percent : ", (nltk.classify.accuracy(SVC_classifier,testing_set))*100)
+
+# LinearSVC
+LinearSVC_classifier = SklearnClassifier(LinearSVC())
+LinearSVC_classifier.train(training_set)
+print("LinearSVC Classifier Algorithm Accuracy Percent : ", (nltk.classify.accuracy(LinearSVC_classifier,testing_set))*100)
+
+# NuSVC
+NuSVC_classifier = SklearnClassifier(NuSVC())
+NuSVC_classifier.train(training_set)
+print("NuSVC Classifier Algorithm Accuracy Percent : ", (nltk.classify.accuracy(NuSVC_classifier,testing_set))*100)
